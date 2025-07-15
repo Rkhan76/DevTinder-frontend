34b782e4e4c4ba3286b5bce66e3f1e0fc553337a
Rakhshan khan <rakshaankhan1@gmail.com>
2025-07-15 18:04:55 +0530

    Refactor Body.jsx layout: feed left, SuggestedFriends right, responsive spacing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,21 @@
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import CreatePost from './CreatePost'
+import Feed from './Feed'
+import SuggestedFriends from './SuggestedFriends'
 
 const Body = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
-      <Footer />
+      <main className="flex flex-col md:flex-row bg-base-200 min-h-screen py-6 w-full justify-center items-start">
+        <div className="w-full max-w-xl flex flex-col items-center min-w-0">
+          <CreatePost />
+          <Feed />
+        </div>
+        <aside className="hidden md:flex md:flex-col md:items-start md:justify-start md:w-[320px] lg:w-[350px] xl:w-[380px] ml-6 mr-6">
+          <SuggestedFriends />
+        </aside>
+      </main>
     </>
   )
 }
